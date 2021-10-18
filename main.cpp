a4e888a02853fe125c2cb2e8cd58fec347f732f9
TheWinner <petergolevski@outlook.com>
2021-10-18 22:02:15 +0300

    Done with exercise 2 & 3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 int main() {
@@ -11,6 +12,41 @@ int main() {
     }
     else {
         cout << "The number " << number << " is countable" << endl;
+    }
+
+    // Exercise 2
+    int year, leap;
+    cout << "Enter a year: ";
+    cin >> year;
+    if (!(year % 100)) {
+        leap = year % 400;
+    }
+    else {
+        leap = year % 4;
+    }
+    if (leap == 0) {
+        cout << year << " is a leap year\n";
+    }
+    else {
+        cout << year << " is not a leap year\n";
+    }
+
+    // Exercise 3
+    double qdna, qdnb, qdnc;
+    double qdex, qdex1, qdex2;
+    cin >> qdna >> qdnb >> qdnc;
+    double discriminant = pow(qdnb , 2) - 4 * qdna * qdnb;
+    if (discriminant < 0) {
+        cout << "No result\n";
+    }
+    else if (discriminant == 0) {
+        qdex = -qdnb / 2 * qdna;
+        cout << qdex;
+    }
+    else {
+        qdex1 = (-qdnb + discriminant) / (2 * qdna);
+        qdex2 = (-qdnb - discriminant) / (2 * qdna);
+        cout << qdex1 << endl << qdex2 << endl;
     }
     return 0;
 }
