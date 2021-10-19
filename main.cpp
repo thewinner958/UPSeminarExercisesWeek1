400da65b9598349008ed290c62620596e5af79b0
TheWinner <petergolevski@outlook.com>
2021-10-19 19:05:12 +0300

    Done with exercise 5

--- main.cpp
+++ main.cpp
@@ -75,5 +75,19 @@ int main() {
             break;
     }
     cout << result << endl;
+
+    // Exercise 5
+    char letter;
+    cin >> letter;
+    if (letter >= 65 && letter <= 90) {
+        letter += 32;
+        cout << "The lower case character corresponding to is " << letter << endl;
+    }
+    else if (letter >= 97 && letter <= 122) {
+        letter -= 32;
+        cout << "The upper case character corresponding to is " << letter << endl;
+    } else {
+        cout << letter << " is not a letter\n";
+    }
     return 0;
 }
